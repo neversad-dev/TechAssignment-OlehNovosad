e08dc9aa81d71e2d8855c9755d0eae887816a7ad
Oleh Novosad <7419136+neversad-dev@users.noreply.github.com>
2025-05-01 14:35:41 +0300

    Rename compose theme

--- app/src/main/java/com/neversad/paymentapp/MainActivity.kt
+++ app/src/main/java/com/neversad/paymentapp/MainActivity.kt
@@ -9,14 +9,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.neversad.paymentapp.navigation.AppNavHost
-import com.neversad.paymentapp.ui.theme.StarterTheme
+import com.neversad.paymentapp.ui.theme.PaymentAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            StarterTheme {
+            PaymentAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     AppNavHost(modifier = Modifier.padding(innerPadding))
                 }

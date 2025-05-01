36e91f6e1147ac27515d67f6a246a8017c92e104
Oleh Novosad <7419136+neversad-dev@users.noreply.github.com>
2025-05-02 00:17:01 +0300

    feat: add Material3 theming and Google Fonts integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.text.googlefonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
